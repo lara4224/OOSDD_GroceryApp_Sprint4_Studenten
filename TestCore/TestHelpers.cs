d0e88f78677c258ca919d85b51be131ede84b431
lara4224 <s1195279@student.windesheim.nl>
2025-10-01 23:56:44 +0200

    EN NU IS HET KLAAR!!!!!!!

--- TestCore/TestHelpers.cs
+++ TestCore/TestHelpers.cs
@@ -2,15 +2,19 @@
 using Grocery.Core.Helpers;
 using NUnit.Framework;
 
+using Grocery.Core.Helpers;
+
 namespace TestCore
 {
-    [TestFixture]
     public class TestHelpers
     {
         [SetUp]
-        public void Setup() { }
+        public void Setup()
+        {
+        }
 
-        // Happy flow - single password
+
+        //Happy flow
         [Test]
         public void TestPasswordHelperReturnsTrue()
         {
@@ -19,7 +23,6 @@ namespace TestCore
             Assert.IsTrue(PasswordHelper.VerifyPassword(password, passwordHash));
         }
 
-        // Happy flow - multiple passwords
         [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08=")]
         [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=")]
         public void TestPasswordHelperReturnsTrue(string password, string passwordHash)
@@ -27,22 +30,21 @@ namespace TestCore
             Assert.IsTrue(PasswordHelper.VerifyPassword(password, passwordHash));
         }
 
-        // Unhappy flow - single test
+
+        //Unhappy flow
         [Test]
         public void TestPasswordHelperReturnsFalse()
         {
-            string invalidPassword = "wrongPassword";
-            string correctHash = "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=";
-            Assert.IsFalse(PasswordHelper.VerifyPassword(invalidPassword, correctHash));
+            string wrongPassword = "wrongpassword";
+            string passwordHash = "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=";
+            Assert.IsFalse(PasswordHelper.VerifyPassword(wrongPassword, passwordHash));
         }
 
-        // Unhappy flow - multiple test cases
         [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08")]
         [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA")]
         public void TestPasswordHelperReturnsFalse(string password, string passwordHash)
         {
-            // Assuming these hashes are incorrect or the password should fail
-            Assert.IsFalse(PasswordHelper.VerifyPassword(password + "wrong", passwordHash));
+            Assert.IsFalse(PasswordHelper.VerifyPassword(password, passwordHash));
         }
     }
 }
