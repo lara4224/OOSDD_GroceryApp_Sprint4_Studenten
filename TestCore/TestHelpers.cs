05fee64b0d86fc9fb3d607c25f9105387c2c5e45
lara4224 <s1195279@student.windesheim.nl>
2025-10-01 21:45:38 +0200

    Removed tests from previous sprint

--- TestCore/TestHelpers.cs
+++ TestCore/TestHelpers.cs
@@ -26,19 +26,5 @@ namespace TestCore
             Assert.IsTrue(PasswordHelper.VerifyPassword(password, passwordHash));
         }
 
-
-        //Unhappy flow
-        [Test]
-        public void TestPasswordHelperReturnsFalse()
-        {
-            Assert.Pass(); //Zelf uitwerken
-        }
-
-        [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08")]
-        [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA")]
-        public void TestPasswordHelperReturnsFalse(string password, string passwordHash)
-        {
-            Assert.Fail(); //Zelf uitwerken zodat de test slaagt!
-        }
     }
 }
